b068884aa62d8034ee0223b01d687e858d9d1bdc
David Whipple <dwhipple99@hotmail.com>
2016-01-16 20:45:17 -0800

    all helper functions for allMovesHelp seem to be working, saved old code

--- SBP/src/Game.java
+++ SBP/src/Game.java
@@ -6,6 +6,7 @@
  * It is intended to implement a Sliding Brick Puzzle
  */
 
+import java.lang.System.*;
 import java.util.*;
 import java.lang.Object;
 import java.io.BufferedReader;
@@ -182,13 +183,272 @@ public class Game {
         return solved;
     }
 
+    /*public static boolean canMoveUp(Game g1, int piece) {
+        if
+    }
+    public static boolean canMoveDown {}
+    public static boolean canMoveLeft {}
+    public static boolean canMoveRight {}
+*/
+
+    public static int pieceWidth(Game g1, int piece){
+
+        int width=1;
+
+        if (piece < 2) {
+            System.out.println("Error: Invalid piece number -- EXITING");
+            System.exit(1);
+        }
+
+        //System.out.println("Board Width="+g1.getWidth());
+        for (int i=0;i<g1.getHeight();i++) {
+            for (int j=0;j<g1.getWidth();j++) {
+                if (g1.board[j][i]==piece) {
+                    //System.out.println("i="+i+" j="+j+" width="+width);
+                    //System.out.println("g1.board[j+width][i]="+g1.board[j+width][i]);
+                    while(g1.board[j+width][i]==piece) {
+                        width++;
+                    }
+                    // Force the loops to end
+                    i=g1.getHeight();
+                    j=g1.getWidth();
+                }
+            }
+        }
+        return width;
+    }
+
+    public static int pieceHeight(Game g1, int piece){
+
+        int height=1;
+
+        if (piece < 2) {
+            System.out.println("Error: Invalid piece number -- EXITING");
+            System.exit(1);
+        }
+
+        for (int i=0;i<g1.getHeight();i++) {
+            for (int j=0;j<g1.getWidth();j++) {
+                if (g1.board[j][i]==piece) {
+                    //System.out.println("i="+i+" j="+j+" width="+width);
+                    //System.out.println("g1.board[j+width][i]="+g1.board[j+width][i]);
+                    while(g1.board[j][i+height]==piece) {
+                        height++;
+                    }
+                    // Force the loops to end
+                    i=g1.getHeight();
+                    j=g1.getWidth();
+                }
+            }
+        }
+        return height;
+    }
+
+    public static boolean canMoveUp(Game g1, int piece, int i, int j, List<Move> listOfMoves){
+        int pWidth, pHeight;
+        int curCol, curRow;
+
+        pWidth=pieceWidth(g1,piece);
+        pHeight=pieceHeight(g1, piece);
+
+        curCol=i;
+        curRow=j;
+
+        // Check if can move up, entire width must be able to move up
+        boolean canMoveUp=true;
+        while ((pWidth >= 1) && (canMoveUp)) {
+            if (g1.board[curRow][curCol-1]!=0){
+                canMoveUp=false;
+            }
+            curRow++;
+            pWidth--;
+        }
+        if (canMoveUp) {
+            Move myMove=new Move(piece, Move.Direction.UP);
+            listOfMoves.add(myMove);
+        }
+        return canMoveUp;
+    }
+
+    public static boolean canMoveDown(Game g1, int piece, int i, int j, List<Move> listOfMoves){
+        int pWidth, pHeight;
+        int curCol, curRow;
+
+        pWidth=pieceWidth(g1,piece);
+        pHeight=pieceHeight(g1, piece);
+
+        curCol=i;
+        curRow=j;
+
+        // Check if can move up, entire width must be able to move up
+        boolean canMoveDown=true;
+        while ((pWidth >= 1) && (canMoveDown)) {
+            if (g1.board[curRow][curCol+1]!=0){
+                canMoveDown=false;
+            }
+            curRow++;
+            pWidth--;
+        }
+        if (canMoveDown) {
+            Move myMove=new Move(piece, Move.Direction.DOWN);
+            listOfMoves.add(myMove);
+        }
+        return canMoveDown;
+    }
+
+    public static boolean canMoveLeft(Game g1, int piece, int i, int j, List<Move> listOfMoves){
+        int pWidth, pHeight;
+        int curCol, curRow;
+
+        pWidth=pieceWidth(g1,piece);
+        pHeight=pieceHeight(g1, piece);
+
+        curCol=i;
+        curRow=j;
+
+        // Check if can move up, entire width must be able to move up
+        boolean canMoveLeft=true;
+        while ((pHeight >= 1) && (canMoveLeft)) {
+            // If above is not empty
+            //System.out.println("g1.board[curRow-1][curCol]="+g1.board[curRow-1][curCol]);
+            if (g1.board[curRow-1][curCol]!=0){
+                canMoveLeft=false;
+            }
+            curCol++;
+            pHeight--;
+        }
+        if (canMoveLeft) {
+            System.out.println("canMoveLeft=true");
+            Move myMove=new Move(piece, Move.Direction.LEFT);
+            listOfMoves.add(myMove);
+        }
+        return canMoveLeft;
+    }
+
+    public static boolean canMoveRight(Game g1, int piece, int i, int j, List<Move> listOfMoves) {
+        int pWidth, pHeight;
+        int curCol, curRow;
+
+        pWidth = pieceWidth(g1, piece);
+        pHeight = pieceHeight(g1, piece);
+
+        curCol = i;
+        curRow = j;
+        boolean canMoveRight = true;
+        while ((pHeight >= 1) && (canMoveRight)) {
+            // If above is not empty
+            System.out.println("g1.board[curRow+1][curCol+1]="+g1.board[curRow+1][curCol]);
+            if (g1.board[curRow+1][curCol] != 0) {
+                canMoveRight = false;
+            }
+            curCol++;
+            pHeight--;
+        }
+        if (canMoveRight) {
+            System.out.println("canMoveRight=true");
+            Move myMove = new Move(piece, Move.Direction.RIGHT);
+            listOfMoves.add(myMove);
+        }
+        return canMoveRight;
+    }
+
     public static void allMovesHelp(Game g1, int piece, List<Move> listOfMoves){
 
-        Move myMove=new Move(2, Move.Direction.DOWN);
-        Move myMove2=new Move(7, Move.Direction.RIGHT);
+        int pWidth, pHeight;
+        System.out.println("Finding moves for piece="+piece+" on game state \""+g1.getName()+"\"");
 
-        listOfMoves.add(myMove);
-        listOfMoves.add(myMove2);
+        //Move myMove=new Move(2, Move.Direction.DOWN);
+        //Move myMove2=new Move(7, Move.Direction.RIGHT);
+
+        for (int i=0;i<g1.getHeight();i++) {
+            for (int j=0;j<g1.getWidth();j++) {
+                if (g1.board[j][i]==piece) {
+                    pWidth=pieceWidth(g1,piece);
+                    pHeight=pieceHeight(g1, piece);
+                    System.out.println("Piece found @ row="+j+", col="+i+", Wid="+pWidth+", Height="+pHeight);
+
+                    boolean moveUp=canMoveUp(g1, piece, i,j,listOfMoves);
+                    boolean moveDown=canMoveDown(g1, piece, i,j,listOfMoves);
+                    boolean moveLeft=canMoveLeft(g1, piece, i,j,listOfMoves);
+                    boolean moveRight=canMoveRight(g1, piece, i,j,listOfMoves);
+
+   /*                 int curCol=i;
+                    int curRow=j;
+
+                    // Check if can move up, entire width must be able to move up
+                    boolean canMoveUp=true;
+                    while ((pWidth >= 1) && (canMoveUp)) {
+                        // If above is not empty
+                     //   System.out.println("g1.board[curRow-1][curCol]="+g1.board[curRow][curCol-1]);
+                        if (g1.board[curRow][curCol-1]!=0){
+                            canMoveUp=false;
+                        }
+                        curRow++;
+                        pWidth--;
+                    }
+                    if (canMoveUp) {
+                        System.out.println("canMoveUp=true");
+                        Move myMove=new Move(piece, Move.Direction.UP);
+                        listOfMoves.add(myMove);
+                    }
+                    else {
+                        System.out.println("canMoveUp=false");
+                    }
+
+                    int curCol=i;
+                    int curRow=j;
+                    pWidth=pieceWidth(g1,piece);
+                    boolean canMoveDown=true;
+                    while ((pWidth >= 1) && (canMoveDown)) {
+                        // If above is not empty
+                        System.out.println("g1.board[curRow][curCol+1]="+g1.board[curRow][curCol+1]);
+                        if (g1.board[curRow][curCol+1]!=0){
+                            canMoveDown=false;
+                        }
+                        curRow++;
+                        pWidth--;
+                    }
+                    if (canMoveDown) {
+                        System.out.println("canMoveDown=true");
+                        Move myMove=new Move(piece, Move.Direction.DOWN);
+                        listOfMoves.add(myMove);
+                    }
+                    else {
+                        System.out.println("canMoveDown=false");
+                    }
+
+                    int curCol=i;
+                    int curRow=j;
+                    pWidth=pieceWidth(g1,piece);
+                    pHeight=pieceHeight(g1,piece);
+                    boolean canMoveLeft=true;
+                    while ((pHeight >= 1) && (canMoveLeft)) {
+                        // If above is not empty
+                        System.out.println("g1.board[curRow-1][curCol]="+g1.board[curRow-1][curCol]);
+                        if (g1.board[curRow-1][curCol]!=0){
+                            canMoveLeft=false;
+                        }
+                        curCol++;
+                        pHeight--;
+                    }
+                    if (canMoveLeft) {
+                        System.out.println("canMoveLeft=true");
+                        Move myMove=new Move(piece, Move.Direction.LEFT);
+                        listOfMoves.add(myMove);
+                    }
+                    else {
+                        System.out.println("canMoveLeft=false");
+                    }
+*/
+                    // Force the loops to end
+                    i=g1.getHeight();
+                    j=g1.getWidth();
+                }
+            }
+        }
+
+       // listOfMoves.add(myMove);
+       // listOfMoves.add(myMove2);
 
     }
 
@@ -231,6 +491,10 @@ public class Game {
         loadGameState(level0, myPath, fileName);
         outputGameState(level0);
 
+        //int piece=3;
+        //System.out.println("Piece Width of piece="+piece+" is "+pieceWidth(level0,piece));
+        //System.out.println("Piece Height of piece="+piece+" is "+pieceHeight(level0,piece));
+/*
         if (gameStateSolved(level0)) {
             System.out.println("Game level0 is solved");
         }
@@ -238,7 +502,7 @@ public class Game {
             System.out.println("Game level0 not solved");
         }
         Game level0Clone=cloneGameState(level0);
-        outputGameState(level0Clone);
+        //outputGameState(level0Clone);
         if(stateEqual(level0, level0Clone)) {
             System.out.println("level0 and level0-clone are equal");
         }
@@ -254,7 +518,7 @@ public class Game {
         myPath="C:/Users/dwhip_000/IdeaProjects//SBP/SBP/data/";
         fileName="SBP-level0-solved.txt";
         loadGameState(level0Solved, myPath, fileName);
-        outputGameState(level0Solved);
+        //outputGameState(level0Solved);
 
         if (gameStateSolved(level0Solved)) {
             System.out.println("Game level0-solved is solved");
@@ -264,11 +528,13 @@ public class Game {
         }
 
         //System.out.println("Move piece="+myMove.getPiece()+", Direction="+myMove.getDirection());
-
+*/
         List<Move> listOfMoves=new ArrayList<Move>();
-        int piece=0;
+        int piece=2;
 
         allMovesHelp(level0, piece, listOfMoves);
+
+        System.out.println("Number of list items="+listOfMoves.size());
 
         ListIterator<Move> itr=listOfMoves.listIterator();
 
@@ -278,13 +544,13 @@ public class Game {
             System.out.println("Move piece="+newMove.getPiece()+", Direction="+newMove.getDirection());
         }
 
-
+/*
         // Load SBP-Level1.txt
         Game level1 = new Game("Level 1", 0, 0);
         myPath="C:/Users/dwhip_000/IdeaProjects//SBP/SBP/data/";
         fileName="SBP-level1.txt";
         loadGameState(level1, myPath, fileName);
-        outputGameState(level1);
+        //outputGameState(level1);
 
         if(stateEqual(level0, level1)) {
             System.out.println("level0 and level1 are equal");
@@ -292,7 +558,7 @@ public class Game {
         else {
             System.out.println("level0 and level1 are NOT equal");
         }
-/*
+
         // Load SBP-Level2.txt
         Game level2 = new Game("Level 2", 0, 0);
         myPath="C:/Users/dwhip_000/IdeaProjects//SBP/SBP/data/";
